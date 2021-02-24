d1342688a71c84a121c2c998e86851af6b903e5c
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-23 17:05:17 -0700

    Completed module on complex changing states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,50 @@
 import React from "react";
 
 function App() {
+  const [fullName, setFullName] = React.useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange() {
+    // const newValue = event.target.value;
+    // const inputName = event.target.name;
+    const { value: newValue, name: inputName } = event.target;
+    //CAUTION: Can't access event wtihin the setFullName function
+
+    setFullName((prevValue) => {
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          onChange={handleChange}
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={handleChange}
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
